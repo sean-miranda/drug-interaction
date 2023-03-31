473dc2d61eda591531fe173cc4c175cca6cfea2f
Sean Miranda <sean@docmode.com>
2023-03-31 13:33:02 -0700

    Add class for FoodInteraction and TherapeuticDuplicaiton clases

--- interaction-logic.py
+++ interaction-logic.py
@@ -18,14 +18,47 @@ class DrugInteraction:
         self.interaction
         self.references = []
 
-    def addSeverity(self, severity :str):
+    def addSeverity(self, severity: str):
         self.severity = severity
 
     def addInteraction(self, interaction: str):
         self.interaction = interaction
 
-    def addReference(self, reference):
+    def addReference(self, reference: str):
         self.references.apppend(reference)
+
+
+class FoodInteraction:
+
+    def __init__(self, drug: str):
+        self.drug = drug
+        self.severity
+        self.interaction
+        self.references = []
+
+    def addSeverity(self, severity: str):
+        self.severity = severity
+
+    def addInteraction(self, interaction: str):
+        self.interaction = interaction
+
+    def addReference(self, reference: str):
+        self.references.apppend(reference)
+
+
+class TherapeuticDuplication:
+
+    def __init__(self, drug1: str, drug2: str):
+        self.drug1 = drug1
+        self.drug2 = drug2
+        self.category = []
+        self.text = []
+
+    def addCategory(self, category: str):
+        self.category.append(category)
+
+    def addReference(self, tex: str):
+        self.text.apppend(text)
 
 
 # files:
@@ -38,6 +71,18 @@ drug_details_csv = 'drug_details.csv'
 drug_pairs_csv = 'drug_pairs.csv'
 live_search_url = 'https://www.drugs.com/js/search/?id=livesearch-interaction'
 interaction_url = 'https://www.drugs.com/interactions-check.php'
+
+drug_interaction_severity_xpath = '(//div[contains(@class,"interactions-reference")])[1]//span[contains(@class,"status")]'
+drug_interaction_interaction_xpath = '((//div[contains(@class,"interactions-reference")])[1]//p)[2]'
+drug_interaction_references_xpath = '(//div[contains(@class,"interactions-reference")])[1]//div[contains(@class,"reference-list")]//li'
+
+food_interaction_drug_xpath = '//h2[text()="Drug and food interactions"]//following::div[1]//h3'
+food_interaction_severity_xpath = '//h2[text()="Drug and food interactions"]//following::div[1]//span'
+food_interacion_inteteracion_xpath = '//h2[text()="Drug and food interactions"]//following::div[@class="interactions-reference"]/p[1]'
+food_interaction_references_xpath = '//h2[text()="Drug and food interactions"]//following::div[@class="interactions-reference"]/div[contains(@class,"reference-list")]'
+
+therapeutic_duplication_category_xpath = '(//div[@class="interactions-reference-wrapper"])[3]//h3'
+therapeutic_duplication_text_xpath = '(//div[@class="interactions-reference-wrapper"])[3]//div[@class="interactions-reference"]//descendant::p[2]'
 
 
 def saveDrugDetails():
@@ -93,7 +138,7 @@ def getTextFromXpath(doc, xpath):
 def getInteraction():
 
     df = pd.read_csv(drug_pairs_csv, sep="|").head(1)
-    
+
     interactionDetails = []
 
     for index, row in df.iterrows():
@@ -103,59 +148,69 @@ def getInteraction():
         html_data = requests.get(url=interaction_url, params={
             'drug_list': drug_1 + ',' + drug_2, 'professional': '1'}).text
 
-
         doc = lxml.html.fromstring(html_data)
 
-        ### Drug interaction
-
-        ## Note: To handle multiple interactions
+        ##########################
+        # Drug interaction
+        ##########################
 
         severities = getTextFromXpath(
-            doc, '(//div[contains(@class,"interactions-reference")])[1]//span[contains(@class,"status")]')
+            doc, drug_interaction_severity_xpath)
         interactions = getTextFromXpath(
-            doc, '((//div[contains(@class,"interactions-reference")])[1]//p)[2]')
-        references = getTextFromXpath(
-            doc, '(//div[contains(@class,"interactions-reference")])[1]//div[contains(@class,"reference-list")]//li')
-        
-        ## Note: To add drug Ids
-        drugInteraction =  DrugInteraction(drug_1, drug_2)
+            doc, drug_interaction_interaction_xpath)
+        referencesList = getTextFromXpath(
+            doc, drug_interaction_references_xpath)
 
-        for severity in severities:
+        if len(severities) > 1:
+            print("Multiple severities found for drug interaction : (",
+                  drug_1, " : ", drug_2)
+
+        if len(interactions) > 1:
+            print("Multiple interactions found for drug interaction : (",
+                  drug_1, " : ", drug_2)
+
+        # Note: To add drug Ids
+
+        drugInteraction = DrugInteraction(drug_1, drug_2)
+
+        for severity, interaction, references in zip(severities, interaction, referencesList):
             drugInteraction.addSeverity(severity)
-
-        for interaction in interactions:
             drugInteraction.addInteraction(interaction)
+            for reference in references:
+                drugInteraction.addReference(reference)
 
-        for reference in references:
-            drugInteraction.addReference(reference)
+        ##########################
+        # Food interaction
+        ##########################
 
+        drugs = getTextFromXpath(doc, food_interaction_drug_xpath)
+        severities = getTextFromXpath(doc, food_interaction_severity_xpath)
+        interactions = getTextFromXpath(
+            doc, food_interacion_inteteracion_xpath)
+        referenceList = getTextFromXpath(
+            doc, food_interaction_references_xpath)
 
-        ### Food interaction
+        for drug, severity, interaction, references in zip(drugs, severities, referenceList):
+            foodInteraction = DrugInteraction(drugs.split()[0])
+            foodInteraction.addSeverity(severity)
+            foodInteraction.addInteraction(interaction)
+            for interaction in interactions:
+                foodInteraction.add(interaction)
 
-        drug = getTextFromXpath(
-            doc, '//h2[text()="Drug and food interactions"]//following::div[1]//h3')
+        ##########################
+        # Therapeutic duplication
+        ##########################
 
-        severity = getTextFromXpath(
-            doc, '//h2[text()="Drug and food interactions"]//following::div[1]//span')
+        categories = getTextFromXpath(
+            doc, therapeutic_duplication_category_xpath)
+        texts = getTextFromXpath(
+            doc, therapeutic_duplication_text_xpath)
 
-        foodInteraction = getTextFromXpath(
-            doc, '//h2[text()="Drug and food interactions"]//following::div[@class="interactions-reference"]/p[1]')
+        therapeuticDuplicaiton = TherapeuticDuplication(drug_1, drug_2)
 
-        references = getTextFromXpath(
-            doc, ' //h2[text()="Drug and food interactions"]//following::div[@class="interactions-reference"]/div[contains(@class,"reference-list")]')
-
-        drug = [x.split()[0] for x in drug]
-
-        ### Therapeutic duplication
-
-        duplicationCategory = getTextFromXpath(
-            doc, '(//div[@class="interactions-reference-wrapper"])[3]//h3')
-
-        duplicaitonText = getTextFromXpath(
-            doc, '(//div[@class="interactions-reference-wrapper"])[3]//div[@class="interactions-reference"]//descendant::p[2]')
-
-        print(duplicationCategory)
-        print(duplicaitonText)
+        for category, text in zip(categories, texts):
+            therapeuticDuplicaiton.category(category)
+            therapeuticDuplicaiton.text(text)
 
 
 if __name__ == "__main__":
